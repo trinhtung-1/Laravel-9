b30e16d77b0ffc4c0b8d17b94db891fa3890bce5
tung <trinhutng95@gmail.com>
2022-07-23 12:38:20 +0700

    init chat chanel

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,11 @@ Route::get('/send-mail', function () {
 });
 
 Route::get('/redis',[TestController::class,'index']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/chat', [App\Http\Controllers\ChatsController::class, 'index']);
+Route::get('/messages', [App\Http\Controllers\ChatsController::class, 'fetchMessages']);
+Route::post('/messages', [App\Http\Controllers\ChatsController::class, 'sendMessage']);
